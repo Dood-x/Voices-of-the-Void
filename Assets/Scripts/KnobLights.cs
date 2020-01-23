7de2c052f8365588c03100b65c67e0a202531e48
Aduel13 <adrianpavelic@gmail.com>
2020-01-24 00:00:43 +0100

    ye

---
+++ Assets/Scripts/KnobLights.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnobLights : MonoBehaviour
+{
+    public Controller controller;
+
+    public GameObject left;
+    public GameObject right;
+    public GameObject buttonLight;
+
+    private void Update()
+    {
+        if(controller.selectedKnob == Controller.Knob.Confirm)
+        {
+            left.SetActive(false);
+            right.SetActive(false);
+            buttonLight.SetActive(true);
+        }
+
+        else if(controller.selectedKnob == Controller.Knob.Vertical)
+        {
+            left.SetActive(true);
+            right.SetActive(false);
+            buttonLight.SetActive(false);
+        }
+
+        else if (controller.selectedKnob == Controller.Knob.Horizontal)
+        {
+            left.SetActive(false);
+            right.SetActive(true);
+            buttonLight.SetActive(false);
+        }
+
+    }
+}
